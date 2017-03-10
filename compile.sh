4632f8e2a07fee27cf85a38a6a7609062b6316cd
guybedford <guybedford@gmail.com>
2017-03-10 12:21:04 +0200

    remove unnecessary fs

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
-clang -S -emit-llvm --target=wasm32 native/fft.c -Oz -c -o fft.bc
-llc -asm-verbose=false -o fft.s fft.bc
-rm fft.bc
+clang -S -emit-llvm --target=wasm32 native/fft.c -Oz -c -o - | llc -asm-verbose=false > fft.s
 s2wasm fft.s > native/fft.wast
 rm fft.s
 wast2wasm native/fft.wast -o native/fft.wasm
